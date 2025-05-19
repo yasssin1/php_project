a5f89942d4251e94fea2b31cc6769f312bccb8a8
yasssin1 <sektiyassine@gmail.com>
2025-05-19 20:03:00 +0100

    added filter to search

--- search.php
+++ search.php
@@ -13,10 +13,22 @@ if (($_GET["search_val"] ?? "") === "") {
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/product.css">
+    <link rel="stylesheet" href="./css/filter.css">
     <title>search</title>
 </head>
 <body>
     <?php include "./php/header.php"; ?>
+        <div class = "filter" >
+        <form action = "search.php">
+            <label>sort by:</label>
+            <select id="sortType" name="sortType">
+            <option value="name">name</option>
+            <option value="priceAC">price acsending</option>
+            <option value="priceDC">price decending</option>
+            </select>
+            <input type = "submit" value="filter">
+        </form>
+    </div>
     <div class = "main">
         <?php include "./php/display.php"; ?>
         <a href="item.html" class = "product">
